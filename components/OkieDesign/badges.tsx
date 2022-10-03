135abce5b9d394e001d1d26e1706dd264e157000
Kenneth Bass <33816169+dishwasher-detergent@users.noreply.github.com>
2022-10-03 15:10:27 -0500

    test

---
+++ components/OkieDesign/badges.tsx
@@ -0,0 +1,14 @@
+const Badges = () => {
+    return (
+        <>
+            <div class="badge xs">Extra Small</div>
+            <div class="badge sm">Small</div>
+            <div class="badge">Standard</div>
+            <div class="badge lg">Large</div>
+            <div class="badge xl">Extra Large</div>
+            <div class="badge xxl">2x Large</div>
+        </>
+    )
+}
+
+export default Badges
