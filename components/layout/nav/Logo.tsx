a38fa650bef5bbdcf87bfc6b373bb4e352b6c121
Kenneth Bass <33816169+dishwasher-detergent@users.noreply.github.com>
2022-10-03 15:51:46 -0500

    fix logo again...

--- components/layout/nav/Logo.tsx
+++ components/layout/nav/Logo.tsx
@@ -13,20 +13,21 @@ const Logo = () => {
   return (
     <Link href="/" passHref>
       <a className="flex h-full cursor-pointer items-center justify-start gap-2 text-xl font-black">
-        <span className="relative h-10 w-24 overflow-hidden">
         {!fallbackImage &&
-          <Image
-            loader={customLoader}
-            src={`${prefix}/static/logo.svg`}
-            alt="Profile Image"
-            width="100%"
-            height="100%"
-            layout="fill"
-            onError={() => {
-              setFallbackImage(true);
-            }}
-          />}
-        </span>
+          <span className="relative h-10 w-24 overflow-hidden">
+            <Image
+              loader={customLoader}
+              src={`${prefix}/static/logo.svg`}
+              alt="Profile Image"
+              width="100%"
+              height="100%"
+              layout="fill"
+              onError={() => {
+                setFallbackImage(true);
+              }}
+            />
+          </span>
+        }
         <span className="hidden md:inline-block">
           {process.env.NEXT_PUBLIC_PROJECT_NAME}
         </span>
